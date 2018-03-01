f9df4ac8c75ac66a9a7355a6b490095158eced4f
scottbelden <scottabelden@gmail.com>
2018-03-01 10:30:40 -0500

    whitespace changes

--- test/multisig/crypto/addresses_test.py
+++ test/multisig/crypto/addresses_test.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from __future__ import absolute_import, division, print_function, \
-  unicode_literals
+    unicode_literals
 
 from unittest import TestCase
 
@@ -11,176 +11,177 @@ from iota.multisig.types import MultisigAddress
 
 
 class MultisigAddressBuilderTestCase(TestCase):
-  """
-  Generating values for this test case using the JS lib:
-
-  .. code-block:: javascript
-
-     // Define digests to use to create the multisig addy.
-     var digests = ['...', ...];
-     ...
-
-     var Multisig = require('./lib/multisig/address.js');
-
-     var addy = new Multisig(digests);
-     console.log(addy.finalize());
-  """
-  # noinspection SpellCheckingInspection
-  def setUp(self):
-    super(MultisigAddressBuilderTestCase, self).setUp()
-
-    # Define some tryte sequences that we can reuse between tests.
-    self.digest_1 =\
-      Digest(
-        trytes =
-          b'FWNEPVJNGUKTSHSBDO9AORBCVWWLVXC9KAMKYYNKPYNJDKSAUURI9ELKOEEYPKVTYP'
-          b'CKOCJQESYFEMINIFKX9PDDGRBEEHYYXCJW9LHGWFZGHKCPVDBGMGQKIPCNKNITGMZT'
-          b'DIWVUB9PCHCOPHMIWKSUKRHZOJPMAY',
-
-        key_index = 0,
-      )
-
-    self.digest_2 =\
-      Digest(
-        trytes =
-          b'PAIRLDJQY9XAUSKIGCTHRJHZVARBEY9NNHYJ9UI9HWWZXFSDWEZEGDCWNVVYSYDV9O'
-          b'HTR9NGGZURISWTNECFTCMEWQQFJ9VKLFPDTYJYXC99OLGRH9OSFJLMEOGHFDHZYEAF'
-          b'IMIZTJRBQUVCR9U9ZWTMUXTUEOUBLC',
-
-        key_index = 0,
-      )
-
-    self.digest_3 =\
-      Digest(
-        trytes =
-          b'KBNYOFY9HJSPBDBFSTIEMYJAAMNOXLVXBDUKJRBUGAPIIZNDARXEWDZRBCIYFQCBID'
-          b'HXIQFIDFPNGIFN9DDXQUGYZGDML9ZIELDSVICFUOPWEPCUWEDUFKXKSOZKTSHIMEIR'
-          b'HOXKPJFRWWCNYPXR9RI9SMBFSDQFWM',
-
-        key_index = 0,
-      )
-
-  def test_success_multiple_digests(self):
     """
-    Generating a multisig address from multiple digests.
+    Generating values for this test case using the JS lib:
+
+    .. code-block:: javascript
+
+       // Define digests to use to create the multisig addy.
+       var digests = ['...', ...];
+       ...
+
+       var Multisig = require('./lib/multisig/address.js');
+
+       var addy = new Multisig(digests);
+       console.log(addy.finalize());
     """
-    builder = MultisigAddressBuilder()
-    builder.add_digest(self.digest_1)
-    builder.add_digest(self.digest_2)
-    addy = builder.get_address()
-
-    self.assertIsInstance(addy, MultisigAddress)
-
     # noinspection SpellCheckingInspection
-    self.assertEqual(
-      addy,
 
-      Address(
-        b'ZYKDKGXTMGINTQLUMVNBBI9XCEI9BWYF9YOPCBFT'
-        b'UUJZWM9YIWHNYZEWOPEVRVLKZCPRKLCQD9BR9FVLC',
-      ),
-    )
+    def setUp(self):
+        super(MultisigAddressBuilderTestCase, self).setUp()
 
-    # The multisig address also keeps track of the digests used to
-    # create it (mostly for troubleshooting purposes).
-    self.assertListEqual(addy.digests, [self.digest_1, self.digest_2])
+        # Define some tryte sequences that we can reuse between tests.
+        self.digest_1 =\
+            Digest(
+                trytes=
+                b'FWNEPVJNGUKTSHSBDO9AORBCVWWLVXC9KAMKYYNKPYNJDKSAUURI9ELKOEEYPKVTYP'
+                b'CKOCJQESYFEMINIFKX9PDDGRBEEHYYXCJW9LHGWFZGHKCPVDBGMGQKIPCNKNITGMZT'
+                b'DIWVUB9PCHCOPHMIWKSUKRHZOJPMAY',
 
-  def test_success_single_digest(self):
-    """
-    Generating a "multisig" address from a single digest.
+                key_index=0,
+            )
 
-    This does the same thing as generating a regular address from the
-    corresponding key.
-    """
-    builder = MultisigAddressBuilder()
-    builder.add_digest(self.digest_1)
-    addy = builder.get_address()
+        self.digest_2 =\
+            Digest(
+                trytes=
+                b'PAIRLDJQY9XAUSKIGCTHRJHZVARBEY9NNHYJ9UI9HWWZXFSDWEZEGDCWNVVYSYDV9O'
+                b'HTR9NGGZURISWTNECFTCMEWQQFJ9VKLFPDTYJYXC99OLGRH9OSFJLMEOGHFDHZYEAF'
+                b'IMIZTJRBQUVCR9U9ZWTMUXTUEOUBLC',
 
-    self.assertIsInstance(addy, MultisigAddress)
+                key_index=0,
+            )
 
-    # noinspection SpellCheckingInspection
-    self.assertEqual(
-      addy,
+        self.digest_3 =\
+            Digest(
+                trytes=
+                b'KBNYOFY9HJSPBDBFSTIEMYJAAMNOXLVXBDUKJRBUGAPIIZNDARXEWDZRBCIYFQCBID'
+                b'HXIQFIDFPNGIFN9DDXQUGYZGDML9ZIELDSVICFUOPWEPCUWEDUFKXKSOZKTSHIMEIR'
+                b'HOXKPJFRWWCNYPXR9RI9SMBFSDQFWM',
 
-      Address(
-        b'TBOLOKTNJ9MFGBSJBIWDZBHWJRLMKAEGUZFJFNGS'
-        b'VODKPPULLGJVHTCENCD9OOCNYPRLV9XGBGLDZNHPZ',
-      ),
-    )
+                key_index=0,
+            )
 
-    # The address is still designated multisig, so we keep track of the
-    # digest used to generate it.
-    self.assertListEqual(addy.digests, [self.digest_1])
+    def test_success_multiple_digests(self):
+        """
+        Generating a multisig address from multiple digests.
+        """
+        builder = MultisigAddressBuilder()
+        builder.add_digest(self.digest_1)
+        builder.add_digest(self.digest_2)
+        addy = builder.get_address()
 
-  def test_error_no_digests(self):
-    """
-    Attempting to generate a multisig addresses without providing any
-    digests.
+        self.assertIsInstance(addy, MultisigAddress)
 
-    I mean, why even bother, right?
-    """
-    builder = MultisigAddressBuilder()
+        # noinspection SpellCheckingInspection
+        self.assertEqual(
+            addy,
 
-    with self.assertRaises(ValueError):
-      builder.get_address()
+            Address(
+                b'ZYKDKGXTMGINTQLUMVNBBI9XCEI9BWYF9YOPCBFT'
+                b'UUJZWM9YIWHNYZEWOPEVRVLKZCPRKLCQD9BR9FVLC',
+            ),
+        )
 
-  def test_success_duplicate_digest(self):
-    """
-    Using the same digest multiple times in the same multisig address?
+        # The multisig address also keeps track of the digests used to
+        # create it (mostly for troubleshooting purposes).
+        self.assertListEqual(addy.digests, [self.digest_1, self.digest_2])
 
-    It's unconventional, admittedly, but the maths work out, so..
-    """
-    builder = MultisigAddressBuilder()
-    builder.add_digest(self.digest_1)
-    builder.add_digest(self.digest_2)
+    def test_success_single_digest(self):
+        """
+        Generating a "multisig" address from a single digest.
 
-    # I have no idea why you'd want to do this, but that's why it's not
-    # my job to make those kinds of decisions.
-    builder.add_digest(self.digest_1)
+        This does the same thing as generating a regular address from the
+        corresponding key.
+        """
+        builder = MultisigAddressBuilder()
+        builder.add_digest(self.digest_1)
+        addy = builder.get_address()
 
-    addy = builder.get_address()
+        self.assertIsInstance(addy, MultisigAddress)
 
-    self.assertIsInstance(addy, MultisigAddress)
+        # noinspection SpellCheckingInspection
+        self.assertEqual(
+            addy,
 
-    # noinspection SpellCheckingInspection
-    self.assertEqual(
-      addy,
+            Address(
+                b'TBOLOKTNJ9MFGBSJBIWDZBHWJRLMKAEGUZFJFNGS'
+                b'VODKPPULLGJVHTCENCD9OOCNYPRLV9XGBGLDZNHPZ',
+            ),
+        )
 
-      Address(
-        b'JXJLZDJENNRODT9VEIRPVDX9YRLMDYDEXCQUYFIU'
-        b'XFKFJOYOGTJPEIBEKDNEFRFVVVSQFBGMNZRBGFARD',
-      ),
-    )
+        # The address is still designated multisig, so we keep track of the
+        # digest used to generate it.
+        self.assertListEqual(addy.digests, [self.digest_1])
 
-    # Note that ``digest_1`` appears twice, because we added it twice.
-    self.assertListEqual(
-      addy.digests,
-      [self.digest_1, self.digest_2, self.digest_1],
-    )
+    def test_error_no_digests(self):
+        """
+        Attempting to generate a multisig addresses without providing any
+        digests.
 
-  def test_success_extract_multiple(self):
-    """
-    You can extract the address multiple times from the same builder
-    (it's the same instance every time).
-    """
-    builder = MultisigAddressBuilder()
-    builder.add_digest(self.digest_1)
-    builder.add_digest(self.digest_2)
-    addy_1 = builder.get_address()
-    addy_2 = builder.get_address()
+        I mean, why even bother, right?
+        """
+        builder = MultisigAddressBuilder()
 
-    # Same instance is returned every time.
-    self.assertIs(addy_1, addy_2)
+        with self.assertRaises(ValueError):
+            builder.get_address()
 
-  def test_error_already_finalized(self):
-    """
-    Once an address is extracted from the builder, no more digests can
-    be added.
-    """
-    builder = MultisigAddressBuilder()
-    builder.add_digest(self.digest_1)
-    builder.add_digest(self.digest_2)
-    builder.get_address()
+    def test_success_duplicate_digest(self):
+        """
+        Using the same digest multiple times in the same multisig address?
 
-    with self.assertRaises(ValueError):
-      builder.add_digest(self.digest_3)
+        It's unconventional, admittedly, but the maths work out, so..
+        """
+        builder = MultisigAddressBuilder()
+        builder.add_digest(self.digest_1)
+        builder.add_digest(self.digest_2)
+
+        # I have no idea why you'd want to do this, but that's why it's not
+        # my job to make those kinds of decisions.
+        builder.add_digest(self.digest_1)
+
+        addy = builder.get_address()
+
+        self.assertIsInstance(addy, MultisigAddress)
+
+        # noinspection SpellCheckingInspection
+        self.assertEqual(
+            addy,
+
+            Address(
+                b'JXJLZDJENNRODT9VEIRPVDX9YRLMDYDEXCQUYFIU'
+                b'XFKFJOYOGTJPEIBEKDNEFRFVVVSQFBGMNZRBGFARD',
+            ),
+        )
+
+        # Note that ``digest_1`` appears twice, because we added it twice.
+        self.assertListEqual(
+            addy.digests,
+            [self.digest_1, self.digest_2, self.digest_1],
+        )
+
+    def test_success_extract_multiple(self):
+        """
+        You can extract the address multiple times from the same builder
+        (it's the same instance every time).
+        """
+        builder = MultisigAddressBuilder()
+        builder.add_digest(self.digest_1)
+        builder.add_digest(self.digest_2)
+        addy_1 = builder.get_address()
+        addy_2 = builder.get_address()
+
+        # Same instance is returned every time.
+        self.assertIs(addy_1, addy_2)
+
+    def test_error_already_finalized(self):
+        """
+        Once an address is extracted from the builder, no more digests can
+        be added.
+        """
+        builder = MultisigAddressBuilder()
+        builder.add_digest(self.digest_1)
+        builder.add_digest(self.digest_2)
+        builder.get_address()
+
+        with self.assertRaises(ValueError):
+            builder.add_digest(self.digest_3)
--- test/multisig/transaction_test.py
+++ test/multisig/transaction_test.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from __future__ import absolute_import, division, print_function, \
-  unicode_literals
+    unicode_literals
 
 from unittest import TestCase
 
@@ -11,147 +11,147 @@ from iota.multisig.types import MultisigAddress
 
 
 class ProposedMultisigBundleTestCase(TestCase):
-  # noinspection SpellCheckingInspection
-  def setUp(self):
-    super(ProposedMultisigBundleTestCase, self).setUp()
-
-    self.bundle = ProposedMultisigBundle()
-
-    # Define some tryte sequences that we can reuse between tests.
-    self.digest_1 =\
-      Digest(
-        trytes =
-          b'FWNEPVJNGUKTSHSBDO9AORBCVWWLVXC9KAMKYYNKPYNJDKSAUURI9ELKOEEYPKVTYP'
-          b'CKOCJQESYFEMINIFKX9PDDGRBEEHYYXCJW9LHGWFZGHKCPVDBGMGQKIPCNKNITGMZT'
-          b'DIWVUB9PCHCOPHMIWKSUKRHZOJPMAY',
-
-        key_index = 0,
-      )
-
-    self.digest_2 =\
-      Digest(
-        trytes =
-          b'PAIRLDJQY9XAUSKIGCTHRJHZVARBEY9NNHYJ9UI9HWWZXFSDWEZEGDCWNVVYSYDV9O'
-          b'HTR9NGGZURISWTNECFTCMEWQQFJ9VKLFPDTYJYXC99OLGRH9OSFJLMEOGHFDHZYEAF'
-          b'IMIZTJRBQUVCR9U9ZWTMUXTUEOUBLC',
-
-        key_index = 0,
-      )
-
-    self.trytes_1 = (
-      b'TESTVALUE9DONTUSEINPRODUCTION99999IIPEM9'
-      b'LA9FLHEGHDACSA9DOBQHQCX9BBHCFDIIMACARHA9B'
-    )
-
-    self.trytes_2 = (
-      b'TESTVALUE9DONTUSEINPRODUCTION99999BGUDVE'
-      b'DGH9WFQDEDVETCOGEGCDI9RFHGFGXBI99EJICHNEM'
-    )
-
-    self.trytes_3 = (
-      b'TESTVALUE9DONTUSEINPRODUCTION99999XBGEUC'
-      b'LF9EIFXHM9KHQANBLBHFVGTEGBWHNAKFDGZHYGCHI'
-    )
-
-  def test_add_inputs_happy_path(self):
-    """
-    Adding a multisig input to a bundle.
-    """
     # noinspection SpellCheckingInspection
-    self.bundle.add_transaction(
-      ProposedTransaction(
-        address = Address(self.trytes_1),
-        value   = 42,
-      ),
-    )
+    def setUp(self):
+        super(ProposedMultisigBundleTestCase, self).setUp()
 
-    self.bundle.add_inputs([
-      MultisigAddress(
-        trytes  = self.trytes_2,
-        digests = [self.digest_1, self.digest_2],
-        balance = 42,
-      )
-    ])
+        self.bundle = ProposedMultisigBundle()
 
-    # The multisig input requires a total of 4 transactions to store
-    # all the signatures.  Including the spend, that makes 5
-    # transactions in total.
-    self.assertEqual(len(self.bundle), 5)
+        # Define some tryte sequences that we can reuse between tests.
+        self.digest_1 =\
+            Digest(
+                trytes=
+                b'FWNEPVJNGUKTSHSBDO9AORBCVWWLVXC9KAMKYYNKPYNJDKSAUURI9ELKOEEYPKVTYP'
+                b'CKOCJQESYFEMINIFKX9PDDGRBEEHYYXCJW9LHGWFZGHKCPVDBGMGQKIPCNKNITGMZT'
+                b'DIWVUB9PCHCOPHMIWKSUKRHZOJPMAY',
 
-  def test_add_inputs_error_already_finalized(self):
-    """
-    Attempting to add inputs to a bundle that is already finalized.
-    """
-    self.bundle.add_transaction(
-      ProposedTransaction(
-        address = Address(self.trytes_1),
-        value   = 0,
-      ),
-    )
+                key_index=0,
+            )
 
-    self.bundle.finalize()
+        self.digest_2 =\
+            Digest(
+                trytes=
+                b'PAIRLDJQY9XAUSKIGCTHRJHZVARBEY9NNHYJ9UI9HWWZXFSDWEZEGDCWNVVYSYDV9O'
+                b'HTR9NGGZURISWTNECFTCMEWQQFJ9VKLFPDTYJYXC99OLGRH9OSFJLMEOGHFDHZYEAF'
+                b'IMIZTJRBQUVCR9U9ZWTMUXTUEOUBLC',
 
-    with self.assertRaises(RuntimeError):
-      self.bundle.add_inputs([])
+                key_index=0,
+            )
 
-  def test_add_inputs_error_digests_empty(self):
-    """
-    Adding a multisig input with unknown digests.
-    """
-    with self.assertRaises(ValueError):
-      self.bundle.add_inputs([
-        MultisigAddress(
-          trytes  = self.trytes_1,
-          digests = [],
-        ),
-      ])
+        self.trytes_1 = (
+            b'TESTVALUE9DONTUSEINPRODUCTION99999IIPEM9'
+            b'LA9FLHEGHDACSA9DOBQHQCX9BBHCFDIIMACARHA9B'
+        )
 
-  def test_add_inputs_error_balance_null(self):
-    """
-    Adding a multisig input with null balance.
-    """
-    with self.assertRaises(ValueError):
-      self.bundle.add_inputs([
-        MultisigAddress(
-          trytes  = self.trytes_1,
-          digests = [self.digest_1, self.digest_2],
-          # balance = 42,
-        ),
-      ])
+        self.trytes_2 = (
+            b'TESTVALUE9DONTUSEINPRODUCTION99999BGUDVE'
+            b'DGH9WFQDEDVETCOGEGCDI9RFHGFGXBI99EJICHNEM'
+        )
 
-  def test_add_inputs_error_multiple(self):
-    """
-    Attempting to add multiple multisig inputs.
+        self.trytes_3 = (
+            b'TESTVALUE9DONTUSEINPRODUCTION99999XBGEUC'
+            b'LF9EIFXHM9KHQANBLBHFVGTEGBWHNAKFDGZHYGCHI'
+        )
 
-    This is not currently supported.
-    """
-    with self.assertRaises(ValueError):
-      # noinspection SpellCheckingInspection
-      self.bundle.add_inputs([
-        MultisigAddress(
-          trytes  = self.trytes_1,
-          digests = [self.digest_1, self.digest_2],
-          balance = 42,
-        ),
+    def test_add_inputs_happy_path(self):
+        """
+        Adding a multisig input to a bundle.
+        """
+        # noinspection SpellCheckingInspection
+        self.bundle.add_transaction(
+            ProposedTransaction(
+                address=Address(self.trytes_1),
+                value=42,
+            ),
+        )
 
-        MultisigAddress(
-          trytes  = self.trytes_2,
-          digests = [self.digest_2, self.digest_1],
-        ),
-      ])
+        self.bundle.add_inputs([
+            MultisigAddress(
+                trytes=self.trytes_2,
+                digests=[self.digest_1, self.digest_2],
+                balance=42,
+            )
+        ])
 
-  def test_add_inputs_error_not_multisig(self):
-    """
-    Attempting to add a non-multisig input.
+        # The multisig input requires a total of 4 transactions to store
+        # all the signatures.  Including the spend, that makes 5
+        # transactions in total.
+        self.assertEqual(len(self.bundle), 5)
 
-    This is not currently supported.
-    """
-    with self.assertRaises(TypeError):
-      # noinspection SpellCheckingInspection,PyTypeChecker
-      self.bundle.add_inputs([
-        Address(
-          trytes    = self.trytes_1,
-          key_index = 0,
-          balance   = 76,
-        ),
-      ])
+    def test_add_inputs_error_already_finalized(self):
+        """
+        Attempting to add inputs to a bundle that is already finalized.
+        """
+        self.bundle.add_transaction(
+            ProposedTransaction(
+                address=Address(self.trytes_1),
+                value=0,
+            ),
+        )
+
+        self.bundle.finalize()
+
+        with self.assertRaises(RuntimeError):
+            self.bundle.add_inputs([])
+
+    def test_add_inputs_error_digests_empty(self):
+        """
+        Adding a multisig input with unknown digests.
+        """
+        with self.assertRaises(ValueError):
+            self.bundle.add_inputs([
+                MultisigAddress(
+                    trytes=self.trytes_1,
+                    digests=[],
+                ),
+            ])
+
+    def test_add_inputs_error_balance_null(self):
+        """
+        Adding a multisig input with null balance.
+        """
+        with self.assertRaises(ValueError):
+            self.bundle.add_inputs([
+                MultisigAddress(
+                    trytes=self.trytes_1,
+                    digests=[self.digest_1, self.digest_2],
+                    # balance = 42,
+                ),
+            ])
+
+    def test_add_inputs_error_multiple(self):
+        """
+        Attempting to add multiple multisig inputs.
+
+        This is not currently supported.
+        """
+        with self.assertRaises(ValueError):
+            # noinspection SpellCheckingInspection
+            self.bundle.add_inputs([
+                MultisigAddress(
+                    trytes=self.trytes_1,
+                    digests=[self.digest_1, self.digest_2],
+                    balance=42,
+                ),
+
+                MultisigAddress(
+                    trytes=self.trytes_2,
+                    digests=[self.digest_2, self.digest_1],
+                ),
+            ])
+
+    def test_add_inputs_error_not_multisig(self):
+        """
+        Attempting to add a non-multisig input.
+
+        This is not currently supported.
+        """
+        with self.assertRaises(TypeError):
+            # noinspection SpellCheckingInspection,PyTypeChecker
+            self.bundle.add_inputs([
+                Address(
+                    trytes=self.trytes_1,
+                    key_index=0,
+                    balance=76,
+                ),
+            ])
